fbf39aa3d0cbf0021cc7d22cab4d421e48d74224
Jose Henrique Pasternak <josehenrique7845@gmail.com>
2022-08-29 20:59:15 -0300

    POO

---
+++ src/poo/Carro.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+public class Carro {
+
+    String tipo;
+    String cor;
+    String placa;
+    int numPortas;
+
+    public String getTipo() {
+        return tipo;
+
+    }
+
+    public void setTipo(String Tipo) {
+        this.tipo = tipo;
+
+    }
+
+    public String getCor(){
+       return cor;
+    }
+    
+    public void setCor(String Cor) {
+        this.cor = cor;
+        
+    }
+    public String getPlaca(){
+         return placa;
+    
+    }
+    
+    public void setPlaca(String Placa) {
+        this.placa = Placa;
+    }
+    
+}
---
+++ src/poo/Pessoa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author jose.pasternak
+ */
+public class Pessoa {
+
+    String nome;
+    String corDoCabelo;
+    String bioTipo;
+    int idade;
+
+    public String getNome() {
+        return nome;
+
+    }
+
+    public void setNome(String Nome) {
+        this.nome = nome;
+    }
+
+    public String getCorDoCabelo() {
+        return corDoCabelo;
+
+    }
+    public void setCorDoCabelo(String CorDoCabelo) {
+        this.corDoCabelo = CorDoCabelo;
+    }
+ public String getBioTipo() {
+        return bioTipo;
+    }
+  
+    public void setBioTipo(String bioTipo) {
+        this.bioTipo = bioTipo;
+    }
+}
